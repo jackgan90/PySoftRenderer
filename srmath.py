6415f5963d4ecc82d7b798c6f0fb0e686407b772
jy04916345 <gloomweihan@gmail.com>
2016-11-07 17:07:56 +0800

    add more math entity

--- srmath.py
+++ srmath.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 #mathematics library
 import math
+import collections
 
 EPSILON = 0.00001
 
@@ -21,6 +22,27 @@ class vec2(object):
 		v.y *= scalar
 		return v
 
+	def __getitem__(self, i):
+		if i == 0:
+			return self.x
+		elif i == 1:
+			return self.y
+		else:
+			raise Exception('vec2 only has 2 components')
+
+	def __setitem__(self, i, val):
+		if i == 0:
+			self.x = val
+		elif i == 1:
+			self.y = val
+		else:
+			raise Exception('vec2 only has 2 components')
+
+	def __iter__(self):
+		yield self.x
+		yield self.y
+		yield self.z
+
 	def dot(self, v):
 		return self.x * v.x + self.y * v.y
 
@@ -32,6 +54,10 @@ class vec2(object):
 	def length(self):
 		return math.sqrt(self.sqrLength)
 
+	@property
+	def negate(self):
+		return vec2(-self.x, -self.y)
+
 	def __repr__(self):
 		return (self.x, self.y).__repr__()
 
@@ -41,6 +67,8 @@ class vec2(object):
 			raise Exception('cannot normalize zero-length vector')
 		self.x /= l
 		self.y /= l
+
+		
 	
 vec2.zero = vec2(0, 0)
 	
@@ -64,6 +92,26 @@ class vec3(object):
 		v.z *= scalar
 		return v
 
+	def __getitem__(self, i):
+		if i == 0:
+			return self.x
+		elif i == 1:
+			return self.y
+		elif i == 2:
+			return self.z
+		else:
+			raise Exception('vec3 only has 3 components')
+
+	def __setitem__(self, i, val):
+		if i == 0:
+			self.x = val
+		elif i == 1:
+			self.y = val
+		elif i == 2:
+			self.z = val
+		else:
+			raise Exception('vec3 only has 3 components')
+
 	def dot(self, v):
 		return self.x * v.x + self.y * v.y + self.z * v.z
 
@@ -80,6 +128,10 @@ class vec3(object):
 	@property
 	def length(self):
 		return math.sqrt(self.sqrLength)
+
+	@property
+	def negate(self):
+		return vec3(-self.x, -self.y, -self.z)
 
 	def normalize(self):
 		l = self.length
@@ -116,6 +168,30 @@ class vec4(object):
 		v.w *= scalar
 		return v
 
+	def __getitem__(self, i):
+		if i == 0:
+			return self.x
+		elif i == 1:
+			return self.y
+		elif i == 2:
+			return self.z
+		elif i == 3:
+			return self.w
+		else:
+			raise Exception('vec4 only has 4 components')
+
+	def __setitem__(self, i, val):
+		if i == 0:
+			self.x = val
+		elif i == 1:
+			self.y = val
+		elif i == 2:
+			self.z = val
+		elif i == 3:
+			self.w = val
+		else:
+			raise Exception('vec4 only has 4 components')
+
 	def __repr__(self):
 		return (self.x, self.y, self.z, self.w).__repr__()
 
@@ -126,6 +202,10 @@ class vec4(object):
 	@property
 	def length(self):
 		return math.sqrt(self.sqrLength)
+
+	@property
+	def negate(self):
+		return vec4(-self.x, -self.y, -self.z, -self.w)
 
 	def normalize(self):
 		l = self.length
@@ -161,15 +241,74 @@ class mat3(object):
 		return mat3(l)
 
 	def __mul__(self, multiplier):
-		if not isinstance(multiplier, mat3):
-			return mat3(map(lambda x : x * multiplier, self.elements))
+		if isinstance(multiplier, mat3):
+			result = mat3()
+			for row in xrange(0, 3):
+				for column in xrange(0, 3):
+					result.elements[3 * column + row] = 0
+					for k in xrange(0, 3):
+						result.elements[3 * column + row] += self.elements[3 * k + row] * multiplier.elements[3 * column + k]
+			return result
+		elif isinstance(multiplier, vec3):
+			result = vec3(0, 0, 0)
+			for i in xrange(0, 3):
+				for j in xrange(0, 3):
+					result[i] += self.elements[3 * j + i] * multiplier[j]
+			return result
+				
+		return mat3(map(lambda x : x * multiplier, self.elements))
+
+	def __getitem__(self, xy):
+		if isinstance(xy, int):
+			return vec3(self.elements[3 * xy], self.elements[3 * xy + 1], self.elements[3 * xy + 2])
+		elif isinstance(xy, tuple):
+			column, row = xy
+			return self.elements[3 * column + row]
 		else:
-			return None
+			raise Exception('mat3 __getitem__ only support int or tuple index')
+
+	def __setitem__(self, xy, val):
+		if isinstance(xy, int):
+			if isinstance(val, collections.Iterable):
+				for i in xrange(3):
+					self.elements[3 * xy + i] = val[i]
+		elif isinstance(xy, tuple):
+			column, row = xy
+			self.elements[3 * column + row] = val
+		else:
+			raise Exception('mat3 __setitem__ only support int or tuple index')
+
 			
 	def transpose(self):
 		self.swapElements(1, 3)
 		self.swapElements(2, 6)
 		self.swapElements(5, 7)
+
+	def getInverseMat(self):
+		det = self.determinant
+		if det == 0:
+			return None
+		else:
+			result = mat3()
+			det = 1.0 / det
+			result.elements[0] = det * (self.elements[4] * self.elements[8] - self.elements[7] * self.elements[5])
+			result.elements[1] = det * (self.elements[7] * self.elements[2] - self.elements[1] * self.elements[8])
+			result.elements[2] = det * (self.elements[1] * self.elements[5] - self.elements[4] * self.elements[2])
+			result.elements[3] = det * (self.elements[6] * self.elements[5] - self.elements[3] * self.elements[8])
+			result.elements[4] = det * (self.elements[0] * self.elements[8] - self.elements[6] * self.elements[2])
+			result.elements[5] = det * (self.elements[3] * self.elements[2] - self.elements[0] * self.elements[5])
+			result.elements[6] = det * (self.elements[3] * self.elements[7] - self.elements[6] * self.elements[4])
+			result.elements[7] = det * (self.elements[6] * self.elements[1] - self.elements[0] * self.elements[7])
+			result.elements[8] = det * (self.elements[0] * self.elements[4] - self.elements[3] * self.elements[1])
+			return result
+
+	@property
+	def determinant(self):
+		result = 0
+		result += self.elements[0] * (self.elements[4] * self.elements[8] - self.elements[5] * self.elements[7])
+		result += self.elements[3] * (self.elements[2] * self.elements[7] - self.elements[1] * self.elements[8])
+		result += self.elements[6] * (self.elements[1] * self.elements[5] - self.elements[2] * self.elements[4])
+		return result
 	
 	def swapElements(self, idx0, idx1):
 		temp = self.elements[idx0]
@@ -186,6 +325,6 @@ class mat3(object):
 			if row < 2:
 				res += '\n'
 		return res
+	
 
 mat3.identity = mat3((1,0,0,0,1,0,0,0,1))
-
