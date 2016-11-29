c9343652ce0827358f0d7209973c1c5260817566
GalaxyRipple <gloomweihan@gmail.com>
2016-11-30 00:46:46 +0800

    fix artifact in multithreaded rendering

--- fragmentprocessor.py
+++ fragmentprocessor.py
@@ -7,15 +7,18 @@ class FragmentProcessor(object):
 	def process(self, rasterData, program):
 		#don't need to use predifined semantics,because vertex shader already
 		#produces what fragment shader needs
+		fs = program.fs.__class__()
 		for uniform in program.fs.uniforms:
+			fs.set_uniform(uniform, getattr(program.fs, uniform, None))
+		for uniform in fs.uniforms:
 			if self.pipeline.has_pipeline_uniform(uniform):
-				program.fs.set_uniform(uniform, self.pipeline.get_pipeline_uniform(uniform))
+				fs.set_uniform(uniform, self.pipeline.get_pipeline_uniform(uniform))
 
 		for varyingName, attr in rasterData.fragmentAttrs.iteritems():
-			if varyingName in program.fs.varyings:
-				program.fs.set_varying(varyingName, attr / rasterData.interpolateParam)
+			if varyingName in fs.varyings:
+				fs.set_varying(varyingName, attr / rasterData.interpolateParam)
 
-		program.fs.run()
-		return program.fs.fragcolor
+		fs.run()
+		return fs.fragcolor
 
 
--- vertexprocessor.py
+++ vertexprocessor.py
@@ -12,25 +12,28 @@ class VertexProcessor(object):
 			# variable semantics are predifined,position is always coordinate in object space
 			# color is vertex color
 			# uv is vertex texture coordinate
-			if 'position' in program.vs.varyings:
+			vs = program.vs.__class__()
+			for uniform in program.vs.uniforms:
+				vs.set_uniform(uniform, getattr(program.vs, uniform, None))
+			if 'position' in vs.varyings:
 				position = srmath.vec4(mesh.vertices[idx * 3], mesh.vertices[idx * 3 + 1], \
 						mesh.vertices[idx * 3 + 2], 1.0)
-				program.vs.set_varying('position', position)
-			if 'color' in program.vs.varyings:
+				vs.set_varying('position', position)
+			if 'color' in vs.varyings:
 				color = srmath.vec3(mesh.colors[idx * 3], mesh.colors[idx * 3 + 1], mesh.colors[idx * 3 + 2])
-				program.vs.set_varying('color', color)
-			if 'uv' in program.vs.varyings:
+				vs.set_varying('color', color)
+			if 'uv' in vs.varyings:
 				uv = srmath.vec2(mesh.uvs[idx * 3], mesh.uvs[idx * 3 + 1])
-				program.vs.set_varying('uv', uv)
+				vs.set_varying('uv', uv)
 
-			for uniform in program.vs.uniforms:
+			for uniform in vs.uniforms:
 				if self.pipeline.has_pipeline_uniform(uniform):
-					program.vs.set_uniform(uniform, self.pipeline.get_pipeline_uniform(uniform))
+					vs.set_uniform(uniform, self.pipeline.get_pipeline_uniform(uniform))
 			
-			program.vs.run()
+			vs.run()
 			rasterInput = RasterizeInput()
-			rasterInput.clipPos = srmath.vec4(program.vs.vspos.x, program.vs.vspos.y, \
-					program.vs.vspos.z, program.vs.vspos.w)
-			rasterInput.vertexAttrs = dict(program.vs.vsattrs)
+			rasterInput.clipPos = srmath.vec4(vs.vspos.x, vs.vspos.y, \
+					vs.vspos.z, vs.vspos.w)
+			rasterInput.vertexAttrs = dict(vs.vsattrs)
 			rasterInputs.append(rasterInput)
 		self.pipeline.triangleProcessQueue.put({'rasterInputs' : rasterInputs, 'mode' : mode, 'wireframeColor' : wireframeColor, 'program' : program})
